92ab52e86a2207fd0126f842f27522afabc1a661
AndersonLe <anderleorellana@gmail.com>
2024-03-17 22:57:53 -0500

    Initial commit

---
+++ src/main/java/com/totospz/eshop/EshopApplication.java
@@ -0,0 +1,13 @@
+package com.totospz.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopApplication.class, args);
+	}
+
+}
